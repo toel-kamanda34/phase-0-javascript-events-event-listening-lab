68273685b1fef18a5bc3e36f488f59dc1cd214a0
tkamanda <toelokemwa@gmail.com>
2024-07-22 11:08:48 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+      // Event handler function
+      console.log('Button clicked');
+    });
+  }
